4525d80a5b658bc5b4de8882414ce3f53598217c
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-10-14 23:21:37 +0200

    Libs added to project

--- apps/e-questrian/src/app/app.component.ts
+++ apps/e-questrian/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonsComponent } from '@sigmafirefox/buttons';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ButtonsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
