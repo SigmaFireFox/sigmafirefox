bcd1c5bfff45454235213073d0e6c80a7d2beb05
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-10-14 23:21:41 +0200

    Libs added to project

---
+++ libs/src/widgets/buttons/buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sff-buttons',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './buttons.component.html',
+  styleUrl: './buttons.component.css',
+})
+export class ButtonsComponent {}
