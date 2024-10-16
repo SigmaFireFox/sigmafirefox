5e99368253c7ebeb6a5c5bfe1c6b6454883a29fc
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-10-16 13:05:22 +0200

    Textarea widget added

---
+++ libs/src/widgets/textarea/textarea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sff-textarea',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './textarea.component.html',
+  styleUrl: './textarea.component.scss',
+})
+export class TextareaComponent {}
