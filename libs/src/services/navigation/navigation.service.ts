1c7c73ebc157468e37adf71d660cf770371ee89b
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-10-16 21:33:38 +0200

    navTo method added to Navigation service

--- libs/src/services/navigation/navigation.service.ts
+++ libs/src/services/navigation/navigation.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RouteRelationshipType } from './navigation.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NavigationService {
+  constructor(private router: Router, protected route: ActivatedRoute) {}
 
-  constructor() { }
+  navTo(routeRelation: RouteRelationshipType, route: string){
+    let param: unknown[] = []
+    switch (routeRelation) {
+      case RouteRelationshipType.Absolute :{
+        param = [`"${route}"`]
+        break
+      }
+      case RouteRelationshipType.Parent :{
+        param = [`"../../${route}"`]
+        break
+      }
+      case RouteRelationshipType.Sibling :{
+        param = [`"../${route}"`]
+        break
+      }
+      case RouteRelationshipType.Child :{
+        param = [`./"${route}"`]
+        break
+      }
+    }
+
+    this.router.navigate(param, {relativeTo: this.route});
+  }
 }
