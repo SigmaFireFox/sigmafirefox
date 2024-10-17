46adc62d483f0bd9171c0e4446f90b8f503aac63
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-10-17 17:18:18 +0200

    Auth and Contact Services added to lib

--- libs/src/services/index.ts
+++ libs/src/services/index.ts
@@ -1,2 +1,6 @@
 export * from './navigation/navigation.service'
 export * from './navigation/navigation.model'
+export * from './authentication/authentication.service'
+export * from './authentication/authentication.model'
+export * from './contact-us/contact-us.service'
+export * from './contact-us/contact-us.model'
