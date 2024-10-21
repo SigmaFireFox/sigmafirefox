58d5eb32ba7360750894ea4837d97ab94c496f3a
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-10-22 00:05:21 +0200

    Multiple key methods added to Auth Service

--- libs/src/services/authentication/authentication.service.ts
+++ libs/src/services/authentication/authentication.service.ts
@@ -1,6 +1,123 @@
 import { Injectable } from '@angular/core';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider
+} from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthenticationService {}
+export class AuthenticationService {
+  auth = getAuth();
+
+  registerWithEmail(email: string, password: string) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Register
+        // const user = userCredential.user;
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(error);
+      });
+  }
+
+  signInWithEmail(email: string, password: string) {
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        // const user = userCredential.user;
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(error);
+      });
+  }
+
+  getSignInUser() {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        // const uid = user.uid;
+        console.log(user);
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  }
+  signOut() {
+    signOut(this.auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  }
+
+  signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(this.auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (!credential) return 
+        // const token = credential.accessToken;
+        // The signed-in user info.
+        // const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // The email of the user's account used.
+        // const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        if (!credential) return 
+        // ...
+      });
+  }
+
+  signInWithFacebook() {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(this.auth, provider)
+  .then((result) => {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    if (!credential) return 
+    // const accessToken = credential.accessToken;
+    // The signed-in user info.
+    // const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // The email of the user's account used.
+    // const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+    if (!credential) return 
+    // ...
+  });
+  }
+
+}
