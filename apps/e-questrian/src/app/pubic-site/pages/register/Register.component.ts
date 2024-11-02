56c46e8a7d4dba5f6f21c79c63b516a6b270604d
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-11-02 13:18:51 +0200

    eQuestrian: Extracting register configs to it own file

--- apps/e-questrian/src/app/pubic-site/pages/register/Register.component.ts
+++ apps/e-questrian/src/app/pubic-site/pages/register/Register.component.ts
@@ -2,33 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   ButtonConfig,
-  ButtonSize,
-  ButtonType,
   ContentComponent,
-  ContentConfig,
-  ContentFontSize,
   HeaderComponent,
-  HeaderConfig,
-  HeaderFontSize,
   ImageComponent,
-  ImageConfig,
 } from '@sigmafirefox/widgets';
 import { FormGroup } from '@angular/forms';
 import {
-  FormConfig,
-  FieldType,
-  LabelPosition,
   FormComponent,
-  FooterConfig,
   FooterComponent,
-  FormButtonAlignment,
 } from '@sigmafirefox/components';
 import { ButtonComponent } from '@sigmafirefox/widgets';
 import {
   AuthenticationService,
-  RouteRelationshipType,
 } from '@sigmafirefox/services';
 import { ThirdPartyAuthProviders } from './register.model';
+import { altActionButtons, buttonContent, footerConfig, formConfig, header, imageConfig, thirdPartyButtons, topContent } from './register.config';
 
 @Component({
   selector: 'app-register',
@@ -47,110 +35,15 @@ import { ThirdPartyAuthProviders } from './register.model';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  imageConfig: ImageConfig = {
-    url: 'register-side-image.png',
-    alt: 'Register page side image',
-    height: '100%',
-    width: '50%',
-    left: '50%',
-  };
-
-  topContent: ContentConfig = {
-    size: ContentFontSize.Large,
-    content: ['Join our eQuestrian community'],
-  };
-
-  header: HeaderConfig = {
-    size: HeaderFontSize.Large,
-    content: 'Register',
-  };
-
-  thirdPartyButtons: ButtonConfig[] = [
-    {
-      name: ThirdPartyAuthProviders.Google,
-      text: 'Continue with Google',
-      type: ButtonType.Secondary,
-      size: ButtonSize.CTA_Large,
-    },
-    // Need to register the app before we add other providers
-    // {
-    //   name: ThirdPartyAuthProviders.Facebook,
-    //   text: 'Continue with Facebook',
-    //   type: ButtonType.Secondary,
-    //   size: ButtonSize.CTA_Large,
-    // }
-  ];
-
-  formConfig: FormConfig = {
-    fields: [
-      {
-        name: 'Field1',
-        config: {
-          type: FieldType.Input,
-          label: 'Email',
-          labelPosition: LabelPosition.Internal,
-        },
-      },
-      {
-        name: 'Field1',
-        config: {
-          type: FieldType.Input,
-          label: 'Password',
-          labelPosition: LabelPosition.Internal,
-        },
-      },
-    ],
-    buttons: [
-      {
-        config: {
-          name: 'register',
-          text: 'Register',
-          type: ButtonType.Primary,
-          size: ButtonSize.CTA_Large,
-        },
-        alignment: FormButtonAlignment.Full,
-        isSubmit: true,
-      },
-    ],
-  };
-
-  buttonContent: ContentConfig = {
-    size: ContentFontSize.Medium,
-    content: ['Already a registered user?'],
-  };
-
-  altActionButtons: ButtonConfig[] = [
-    {
-      name: 'sign-in',
-      text: 'Sign in',
-      type: ButtonType.Secondary,
-      size: ButtonSize.CTA_Large,
-    },
-  ];
-
-  footerConfig: FooterConfig = {
-    navlinks: [
-      {
-        relationship: RouteRelationshipType.Parent,
-        route: '',
-        content: { size: ContentFontSize.Medium, content: ['Features'] },
-      },
-      {
-        relationship: RouteRelationshipType.Parent,
-        route: '',
-        content: { size: ContentFontSize.Medium, content: ['Testimonials'] },
-      },
-      {
-        relationship: RouteRelationshipType.None,
-        route: 'contact',
-        content: { size: ContentFontSize.Medium, content: ['Contact us'] },
-      },
-    ],
-    content: {
-      size: ContentFontSize.Small,
-      content: ['© 2024 SigmaFireFox. All rights reserved.'],
-    },
-  };
+  // Configs
+  imageConfig = imageConfig
+  topContent = topContent
+  header = header
+  thirdPartyButtons = thirdPartyButtons
+  formConfig = formConfig
+  buttonContent = buttonContent
+  altActionButtons = altActionButtons
+  footerConfig = footerConfig
 
   constructor(private auth: AuthenticationService) {}
 
