a35bb8270a61f3943b7a63bc32ed8d7aac2537b3
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-11-12 16:47:27 +0200

    eQuestrian : Profile card to platform navbar

---
+++ apps/e-questrian/src/app/platform/modules/profile-management/component/profile-management.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile-management',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-management.component.html',
+  styleUrl: './profile-management.component.scss',
+})
+export class ProfileManagementComponent implements OnInit {
+
+  ngOnInit(){
+    console.log('ProfileManagementComponent')
+  }
+}
---
+++ apps/e-questrian/src/app/platform/modules/profile-management/profile-management.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileManagementRoutingModule } from './profile-management-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProfileManagementRoutingModule
+  ]
+})
+export class ProfileManagementModule { }
