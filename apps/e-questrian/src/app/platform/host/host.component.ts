b5ad160497fb5ee846388fd43dd84301302506f5
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-11-12 14:17:14 +0200

    Platform host component generated and basic routing added to platform

---
+++ apps/e-questrian/src/app/platform/host/host.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-host',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './host.component.html',
+  styleUrl: './host.component.scss',
+})
+export class HostComponent {}
