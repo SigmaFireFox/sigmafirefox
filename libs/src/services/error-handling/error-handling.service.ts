c549b0f05d4945f2f288ed872e17688462bc143b
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-11-12 12:07:44 +0200

    Adding required additional error configs for Registation page

--- libs/src/services/error-handling/error-handling.service.ts
+++ libs/src/services/error-handling/error-handling.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { FirebaseError } from './error-handling.modal';
 import { ErrorModalConfig } from './error-modal.model';
+import {
+  ErrorFirebaseEmailInUse,
+  ErrorFirebaseGeneral,
+  ErrorFirebaseInvalidEmail,
+  ErrorFirebaseMissingPassword,
+  ErrorFirebaseWeakPassword,
+  ErrorGeneral,
+} from './error-handling.const';
 
 @Injectable({
   providedIn: 'root',
@@ -13,34 +21,23 @@ export class ErrorHandlingService {
     if (error.name === 'FirebaseError') {
       switch (error.message as FirebaseError) {
         case FirebaseError.InvalidEmail: {
-          this.errorModalConfig.next({
-            heading: 'Invalid email',
-            content: [
-              'It seems our Authentication service considers \
-              the email address you provided as invalid',
-              'Please check that the email address you have \
-              entered is correct',
-              'If you are certain the address is correct then \
-              please reach out to us via out Contact us form',
-            ],
-          });
-          return;
+          return this.errorModalConfig.next(ErrorFirebaseInvalidEmail);
+        }
+        case FirebaseError.EmailInUse: {
+          return this.errorModalConfig.next(ErrorFirebaseEmailInUse);
+        }
+        case FirebaseError.MissingPassword: {
+          return this.errorModalConfig.next(ErrorFirebaseMissingPassword);
+        }
+        case FirebaseError.WeakPassword: {
+          return this.errorModalConfig.next(ErrorFirebaseWeakPassword);
         }
         default: {
-          this.errorModalConfig.next({
-            heading: 'Firebase error',
-            content: [
-              'It seems Firebase has returned an unspecific \
-              error',
-              'Please retrying submitting the form again',
-              'If this issue persists please reach out to us \
-              via out Contact us form'
-            ],
-          });
-          return;
+          return this.errorModalConfig.next(ErrorFirebaseGeneral);
         }
       }
+    } else {
+      return this.errorModalConfig.next(ErrorGeneral);
     }
-    console.log('This is a General error');
   }
 }
