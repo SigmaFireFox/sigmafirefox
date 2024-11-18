bdde1421d2fb511ce44b1d790a4b34e1630d884a
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-11-18 16:44:52 +0200

    Generating required modules

---
+++ apps/e-questrian/src/app/platform/modules/client-management/clients.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientManagementRoutingModule } from './clients-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClientManagementRoutingModule
+  ]
+})
+export class ClientManagementModule { }
