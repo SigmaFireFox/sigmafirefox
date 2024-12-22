e9dde823c71beb5b3928d0fba27272d0bb865a5a
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-12-22 12:40:41 +0200

    SFF: Generate form row component

---
+++ libs/src/components/form-row/form-row.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sff-form-row',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-row.component.html',
+  styleUrl: './form-row.component.scss',
+})
+export class FormRowComponent {}
