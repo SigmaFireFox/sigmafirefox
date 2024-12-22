73212cf18ba35dbe4bd13a287bcc99ccb9e8dba6
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-12-22 23:43:21 +0200

    SFF: Transform Form Component to Single Column Form Component

---
+++ libs/src/components/form/forms.model.ts
@@ -0,0 +1,24 @@
+import { ButtonConfig } from '../../widgets/button/button.model';
+import { FieldConfig } from "../field/field.model";
+
+export enum FormButtonAlignment {
+  Left,
+  Centre,
+  Right,
+  Full,
+}
+
+export interface FormField {
+  name: string
+  config: FieldConfig
+}
+
+export interface FormButton {
+  config: ButtonConfig
+  isSubmit?: boolean
+  alignment: FormButtonAlignment
+}
+export interface SingleColumnFormConfig {
+  fields: FormField[]
+  buttons: FormButton[]
+}
