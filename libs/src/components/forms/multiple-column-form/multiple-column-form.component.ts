1cc255e684a727165ec31ef94f99bb8be74f43be
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-12-23 00:02:01 +0200

    SFF: Generate Multiple Column Form Component

---
+++ libs/src/components/forms/multiple-column-form/multiple-column-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sff-multiple-column-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './multiple-column-form.component.html',
+  styleUrl: './multiple-column-form.component.scss',
+})
+export class MultipleColumnFormComponent {}
